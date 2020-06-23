970f1f035805aa7c4b7340200b2480744f9c2e12
KySo4oK <prostorostuk08@gmail.com>
2020-06-24 00:25:53 +0300

    Revert "add mvc config and rename page to user controller"

---
+++ src/main/java/extclasses/final_project_spring/controller/PageController.java
@@ -0,0 +1,66 @@
+package extclasses.final_project_spring.controller;
+
+import extclasses.final_project_spring.dto.UserDTO;
+import extclasses.final_project_spring.service.UserService;
+import extclasses.final_project_spring.util.Validator;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Log4j2
+@Controller
+public class PageController {
+    final
+    UserService userService;
+
+    public PageController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String getMainPage() {
+        return "main.html";
+    }
+
+    @GetMapping("/reg")
+    public String getRegPage() {
+        return "reg.html";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login.html";
+    }
+
+    @PostMapping("/reg")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void getNewUser(@RequestBody UserDTO userDTO) {
+        log.info("{}", userDTO);
+        Validator.checkRegistration(userDTO);
+        userService.setNewUser(userDTO);
+    }
+
+    @GetMapping("/prospectus")
+    public String getProspectusPage() {
+        return "prospectus.html";
+    }
+
+    @GetMapping("/orders")
+    public String getOrdersPage() {
+        return "orders.html";
+    }
+
+    @GetMapping("/user")
+    public String getUserPage() {
+        return "user.html";
+    }
+
+    @GetMapping("/book")
+    public String getAddBookPage() {
+        return "bookManagePage.html";
+    }
+}
