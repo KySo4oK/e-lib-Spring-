d735cee75e40dc91a6df64f3251f7f7bdbce59f1
KySo4oK <prostorostuk08@gmail.com>
2020-06-25 22:53:08 +0300

    add catching and showing custom errors in front-end

--- src/main/java/extclasses/final_project_spring/util/CustomExceptionHandler.java
+++ src/main/java/extclasses/final_project_spring/util/CustomExceptionHandler.java
@@ -3,13 +3,14 @@ package extclasses.final_project_spring.util;
 import extclasses.final_project_spring.exception.CustomException;
 import extclasses.final_project_spring.exception.IncorrectDataException;
 import org.springframework.context.MessageSource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
@@ -20,8 +21,9 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler({IncorrectDataException.class, CustomException.class})
-    public final ResponseEntity<String> handleAllExceptions(Exception ex, Locale locale) {
-        return new ResponseEntity<>(messageSource.getMessage(ex.getMessage(), null, locale),
-                HttpStatus.INTERNAL_SERVER_ERROR);
+    public final ResponseEntity<Map<String, String>> handleCustomExceptions(Exception ex, Locale locale) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", messageSource.getMessage(ex.getMessage(), null, locale));
+        return ResponseEntity.badRequest().body(response);
     }
 }
