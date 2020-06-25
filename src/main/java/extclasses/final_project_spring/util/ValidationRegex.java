988fbb64320005e8caa9b5c384fda83bbd4d7941
KySo4oK <prostorostuk08@gmail.com>
2020-06-25 23:00:20 +0300

    remove unused validation code

--- src/main/java/extclasses/final_project_spring/util/ValidationRegex.java
+++ src/main/java/extclasses/final_project_spring/util/ValidationRegex.java
@@ -1,10 +1,6 @@
 package extclasses.final_project_spring.util;
 
 public interface ValidationRegex {
-    String usernameRegex = "^(?=.{5,20}$)(?![_.])(?!.*[_.]{2})[а-щА-ЩЬьЮюЯяЇїІіЄєҐґa-zA-Z0-9._]+(?<![_.])$";
-    String passwordRegex = "^.{4,30}$";
-    String phoneRegex = "^[+]*[(]?[0-9]{1,4}[)]?[-\\s./0-9]*$";
-    String emailRegex = "^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$";
     String bookName = "^[a-zA-Z0-9._]{2,30}$";
     String bookNameUa = "^[а-щА-ЩЬьЮюЯяЇїІіЄєҐґ0-9._]{2,30}$";
 }
--- src/main/java/extclasses/final_project_spring/util/Validator.java
+++ src/main/java/extclasses/final_project_spring/util/Validator.java
@@ -7,16 +7,6 @@ import extclasses.final_project_spring.exception.IncorrectDataException;
 import java.util.regex.Pattern;
 
 public class Validator {
-    public static void checkRegistration(UserDTO userDTO) {
-        if (Pattern.matches(ValidationRegex.usernameRegex, userDTO.getUsername())
-                && Pattern.matches(ValidationRegex.passwordRegex, userDTO.getPassword())
-                && Pattern.matches(ValidationRegex.phoneRegex, userDTO.getPhone())
-                && Pattern.matches(ValidationRegex.emailRegex, userDTO.getEmail())) {
-            return;
-        }
-        throw new IncorrectDataException("bad.credentials");
-    }
-
     public static void checkNewBook(BookDTO bookDTO) {
         if (Pattern.matches(ValidationRegex.bookName, bookDTO.getName()) &&
                 Pattern.matches(ValidationRegex.bookNameUa, bookDTO.getNameUa()) &&
